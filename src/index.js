084f3d901d378facb55ae0c6129b47fbb53e85b5
Oksana Tkachenko <oksana@crystax.net>
2018-09-24 16:24:38 +0300

    installed jquery validation and added started script,WIP

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import $ from 'jquery';
 import 'bootstrap';
+import jqueryValidation from 'jquery-validation';
 
 $(document).ready(function() {
   $('#buttonModal').click(function() {
     setTimeout(function() {
       $('#modal').modal('show');
       }, 2000);
+  });
+
+  /* validation */
+  $('#formModal').validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2
+      },
+      phone: {
+        required: true,
+      },
+      email: {
+        required: true,
+      }
+    }
   });
 });
 
